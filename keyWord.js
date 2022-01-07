02e5fa5449b53464b19b18f4e6f81a1b51a81be2
refat5 <mdrefat382139@gmail.com>
2022-01-07 20:44:04 +0600

    opratior learn

---
+++ keyWord.js
@@ -0,0 +1,62 @@
+//this is for var
+// var a = 6;
+// if (true) {
+//     var a =4;
+//     document.getElementById('keyvar2').innerHTML = a;
+// }
+
+// document.getElementById('keyvar1').innerHTML = a;
+
+// //this is for let keyword 
+
+// let b = 6;
+// if (true) {
+//     let c =43;
+//     document.getElementById('keylet1').innerHTML = c;
+// }
+
+// document.getElementById('keylet2').innerHTML = b;
+
+//Hosisting 
+
+// carName = "star line";
+// var carName;
+// document.getElementById('keyvar1').innerHTML = carName;
+// let myName;
+// myName = "refat";
+
+// document.getElementById('keyvar2').innerHTML = myName;
+
+// let a; 
+// a = 4; //assign;
+// a = 5; //reasign ;
+
+// const a = 4;
+// document.getElementById('keyvar1').innerHTML = a;
+
+//const array
+//reassign not Possible in const  
+
+// const a = [3,5,4,6];
+//  a[3] = 5;
+// document.getElementById('keyvar2').innerHTML =a;
+
+// const b  = 4;
+
+
+//javascript oprator
+
+// concatinate
+// let fname = 'abdur';
+// let lname = 'rahim';
+// let fullname = fname + ' ' + lname;
+
+// let test1 = "You are ";
+// test1 += "beautiful";
+// document.getElementById('keyvar2').innerHTML = test1;
+
+let x = 3 + 3;
+let y = '5' + 3;
+let z = 'hello' + 4;
+
+document.getElementById('keyvar1').innerHTML = typeof x;
