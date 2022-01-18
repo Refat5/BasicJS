634aa71b92323698ebefbabef015c1ef689a2dd0
refat5 <mdrefat382139@gmail.com>
2022-01-18 21:44:31 +0600

    java script datatype

---
+++ dataType.js
@@ -0,0 +1,21 @@
+// let x = 14 + "refat";
+// let x = 16 + 4 + "Volvo";
+// let x =  "Volvo" + 6 + 4 ;
+
+// let x ;
+// let x = 43;
+// let x = "refat";
+
+// let answer = "it's me Refat";
+// let answer2 = 'hello this is me "Refat" ';
+
+// let r = 32-43;
+ 
+// js Array 
+// const name = ['refat' ,'tanvir', 'durjoy'];
+// document.getElementById('datas').innerHTML =  name;
+
+//js Object {}
+
+// const person = {name : "refat" , age : 23 , height: "5'4"}
+// document.getElementById('datas').innerHTML = person.name;
