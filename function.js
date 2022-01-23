5a7b8e1e4a1710aa1a413272464df8cb6403f878
refat5 <mdrefat382139@gmail.com>
2022-01-24 01:14:02 +0600

    function overview

---
+++ function.js
@@ -0,0 +1,36 @@
+// function sleep($name,$time) {
+//     console.log($name +" is a good boy" + $time);
+//     return $name +" is a good boy" + $time;
+// }
+
+// sleep('Refat', '10pm');
+// sleep('Tanvir', '3pm');
+// sleep('Fahad', '10pm');
+// sleep('Durjoy', '4pm');
+
+
+// function sumValue(a ,b){
+//     // return a;
+//     console.log(a+b);
+//     // by default return undefined
+// }
+
+//  let hi = sumValue(34,43);
+
+let carname = 'jdl';
+var carName = 'kds';
+function carFunction(){
+    let carname = 'hanif';
+    var carName = "starline";
+    console.log(carname);
+    console.log(carName);
+
+}
+carFunction();
+
+console.log(carname);
+console.log(carName);
+
+
+
+//document.getElementById('fun').innerHTML = sleep('jdk','32');
