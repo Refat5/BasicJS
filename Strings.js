e739e437f70bb4d2f7b9e06c73d70698d03f8e29
refat5 <mdrefat382139@gmail.com>
2022-02-04 22:55:51 +0600

    Learn Javascript Strings

---
+++ Strings.js
@@ -0,0 +1,20 @@
+// let description = "HEy refat         what's         up"
+
+// document.getElementById('str').innerHTML = description.length;
+
+
+//Eschape characrer
+
+// let Text = "This is Me \"Refat\". you"
+ //let Text = "This is Me \\Refat\\. you"
+
+
+
+ 
+ //String can be object 
+
+//  let r = "refat";
+//  let e = new String("refat");
+
+//  document.getElementById('str').innerHTML= r == e
+//  document.getElementById('str').innerHTML= r === e
