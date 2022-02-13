f40b124a841f9738dfe46f01e23e1a0c5db53eb9
refat5 <mdrefat382139@gmail.com>
2022-02-14 01:30:48 +0600

    learn js number

---
+++ number.js
@@ -0,0 +1,8 @@
+let a = "23";
+let b = 29;
+//string always first priority
+let c = "this is my number"+ a + b;
+let d = a + b +"hey number";
+// document.getElementById('num').innerHTML = isNaN(b);
+
+document.getElementById('num').innerHTML = d;
