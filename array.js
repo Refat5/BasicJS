ac45db489378a72efd22c71cc2e228bfe6dc443e
refat5 <mdrefat382139@gmail.com>
2022-02-21 22:05:13 +0600

    learn somthing new in array

---
+++ array.js
@@ -0,0 +1,59 @@
+// const  myarray = ['refat','tanvir','durjoy','kahlid'];
+
+// const name  = ['refat','tanvir','durjoy','kahlid'];
+// name[4] = 'mamub';
+
+// //find last langht of array 
+
+
+// document.getElementById('array').innerHTML = name[name.length-2];
+
+
+const name1 = ['refat','tanvir','durjoy','kahlid'];
+
+// let fLen = name1.length;
+
+// let text_ul= "<ul>";
+// // for (let i = 0; i < fLen; i++) {
+// //   
+// // }
+
+// //for each array method
+
+// function myFunction(value){
+//   document.getElementById('array').innerHTML = text_ul += "<li>" + value + "</li>";;
+
+// }
+// name1.forEach(myFunction);
+
+
+//Array add element using push method 
+// const  fruits = ['Orange','Apple','Banana'];
+
+// fruits.push('lemon');
+
+// document.getElementById('array').innerHTML = fruits;
+
+
+// Creates undefined "holes" in fruits
+
+// const  fruits = ['Orange','Apple','Banana'];
+
+//  fruits[8]='lemon';
+//  console.log (fruits);
+//  document.getElementById('array').innerHTML = fruits;
+
+
+// Associative Array
+
+// const person = [];
+// person[0] = "Refat";
+// person[1] = "Vai";
+// person[2] = 46;
+
+// const pointsa = new Array(40, 100, 1, 5, 25, 10);
+// const points = [40, 100, 1, 5, 25, 10];
+
+const number = [3,32,34,443];
+const person = {name:'refat',age:32}
+document.getElementById('array').innerHTML =typeof number;    
